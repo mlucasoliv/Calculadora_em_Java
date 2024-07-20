ee710e63ed18aced69c501637a5e35685c2b183a
Marcelo <mlucasdev@hotmail.com>
2024-07-20 20:33:27 -0300

    Atualizado

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,64 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+
+        double resultado = 0;
+
+        System.out.println("======Calculadora======");
+        System.out.println();
+
+        System.out.println("Digite o primeiro número: ");
+        double primeiro = sc.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        double segundo = sc.nextDouble();
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Digite o número da operação que vc deseja: ");
+        System.out.println();
+        System.out.println("1- Somar");
+        System.out.println("2- Subtrair");
+        System.out.println("3- Dividir");
+        System.out.println("4- Multiplicar");
+        int escolha = sc.nextInt();
+        System.out.println();
+        System.out.println();
+
+        switch (escolha){
+
+            case 1:
+                resultado = primeiro + segundo;
+                break;
+            case 2:
+                resultado = primeiro - segundo;
+                break;
+            case 3:
+                    if (primeiro != 0 && segundo != 0) {
+                        resultado = primeiro / segundo;
+                    }
+                    else {
+                        System.out.println("Divisão inválida.");
+                        sc.close();
+                        return;
+                    }
+                    break;
+            case 4:
+                resultado = primeiro * segundo;
+                break;
+            default:
+                System.out.println("Operação inválida.");
+                sc.close();
+                return;
+
         }
+
+        System.out.printf("O resultado da operação é: %.2f", resultado);
+        sc.close();
+
+
     }
 }
